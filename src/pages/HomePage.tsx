74b6920e69d576e472fa0c0a69d8cd220be9e4fb
Harrison <imthatguyh9@gmail.com>
2024-03-05 23:27:21 +1300

    starting to make my website

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a brief introduction about myself.</p>
+    </div>
+  );
+};
+
+export default HomePage;
